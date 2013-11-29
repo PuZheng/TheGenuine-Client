918488eaf6de62139589ec4f85ea4842182a0af8
abc549825 <abc549825@163.com>
2013-11-29 17:37:42 +0800

    fix #24 TheGenuine-Client-24 发表评论， 支持选择星级

--- TheGenuine/src/main/java/com/puzheng/the_genuine/CommentActivity.java
+++ TheGenuine/src/main/java/com/puzheng/the_genuine/CommentActivity.java
@@ -1,6 +1,7 @@
 package com.puzheng.the_genuine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Pair;
@@ -9,8 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
+import com.puzheng.the_genuine.data_structure.Comment;
+import com.puzheng.the_genuine.data_structure.VerificationInfo;
 import com.puzheng.the_genuine.netutils.WebService;
 import com.puzheng.the_genuine.utils.Misc;
+import com.puzheng.the_genuine.utils.PoliteBackgroundTask;
 
 /**
  * Created by abc549825@163.com(https://github.com/abc549825) at 11-29.
@@ -19,6 +23,7 @@ public class CommentActivity extends Activity {
     private int mProductID;
     private EditText mEditText;
     private RatingBar mRatingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,37 +47,30 @@ public class CommentActivity extends Activity {
                 }
                 if (Misc.isEmptyString(comment)) {
                     Toast.makeText(CommentActivity.this, R.string.add_comment_empty, Toast.LENGTH_SHORT).show();
-                } else {
-                    new NewCommentTask().execute(new Pair<String, Float>(comment, mRatingBar.getRating()));
+                    return;
                 }
+                PoliteBackgroundTask.Builder<Boolean> builder = new PoliteBackgroundTask.Builder<Boolean>(CommentActivity.this);
+                builder.msg("正在提交");
+                final String finalComment = comment;
+                builder.run(new PoliteBackgroundTask.XRunnable<Boolean>() {
+                    @Override
+                    public Boolean run() throws Exception {
+                        return WebService.getInstance(CommentActivity.this).addComment(mProductID, finalComment, mRatingBar.getRating());
+                    }
+                });
+                builder.after(new PoliteBackgroundTask.OnAfter<Boolean>() {
+                    @Override
+                    public void onAfter(Boolean aBoolean) {
+                        if (aBoolean) {
+                            Toast.makeText(CommentActivity.this, R.string.add_comment_succeed, Toast.LENGTH_SHORT).show();
+                            CommentActivity.this.finish();
+                        } else {
+                            Toast.makeText(CommentActivity.this, R.string.add_comment_failure, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                builder.create().start();
             }
         });
     }
-
-    class NewCommentTask extends AsyncTask<Pair<String, Float>, Void, Boolean> {
-
-        @Override
-        protected Boolean doInBackground(Pair<String, Float>... params) {
-            try {
-                String comment = params[0].first;
-                float rating = params[0].second;
-                return WebService.getInstance(CommentActivity.this).addComment(mProductID, comment, rating);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            if (aBoolean) {
-                Toast.makeText(CommentActivity.this, R.string.add_comment_succeed, Toast.LENGTH_SHORT).show();
-                CommentActivity.this.finish();
-            }else{
-                Toast.makeText(CommentActivity.this, R.string.add_comment_failure, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-
 }
