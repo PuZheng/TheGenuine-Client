9329916862c222f1103b3404532a03affb9f6647
xiechao06 <xiechao06@gmail.com>
2013-12-02 18:08:21 +0800

    fix #11

--- TheGenuine/src/main/java/com/puzheng/the_genuine/AccountSettingsActivity.java
+++ TheGenuine/src/main/java/com/puzheng/the_genuine/AccountSettingsActivity.java
@@ -1,8 +1,13 @@
 package com.puzheng.the_genuine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.puzheng.the_genuine.views.NavBar;
 
 public class AccountSettingsActivity extends Activity {
 
@@ -10,6 +15,19 @@ public class AccountSettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyApp.unsetCurrentUser();
+                Intent intent = new Intent(AccountSettingsActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        NavBar navBar = (NavBar) findViewById(R.id.navBar);
+        navBar.setContext(this);
     }
 
 
