e7ef5e193e8fcc83d0121252f1822326614d7c97
xiechao06 <xiechao06@gmail.com>
2013-12-04 14:39:57 +0800

    temp

--- TheGenuine/src/main/java/com/puzheng/the_genuine/CounterfeitActivity.java
+++ TheGenuine/src/main/java/com/puzheng/the_genuine/CounterfeitActivity.java
@@ -1,8 +1,13 @@
 package com.puzheng.the_genuine;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.puzheng.the_genuine.views.NavBar;
 
 public class CounterfeitActivity extends Activity {
 
@@ -10,6 +15,18 @@ public class CounterfeitActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counterfeit);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CounterfeitActivity.this, CategoriesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        NavBar navBar = (NavBar) findViewById(R.id.navBar);
+        navBar.setContext(this);
     }
 
 
