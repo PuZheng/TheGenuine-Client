03c0f41532b009f3e8e6e6a9c621930c8d5d8154
abc549825 <abc549825@163.com>
2013-12-10 17:06:51 +0800

    fix #69 实现登录

--- TheGenuine/src/main/java/com/puzheng/the_genuine/data_structure/User.java
+++ TheGenuine/src/main/java/com/puzheng/the_genuine/data_structure/User.java
@@ -1,10 +1,12 @@
 package com.puzheng.the_genuine.data_structure;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by xc on 13-11-29.
  */
 public class User {
-    private final int id;
+    @SerializedName("user_id") private final int id;
     private final String name;
     private final String token;
 
